aa148c5d4bde0500257736030c326a965a178796
Мелася <poliuha.pn@ucu.edu.ua>
2026-04-07 00:12:47 +0300

    Added docstrings for two modules

--- Implement_Queue_using_Stacks/Implement_Queue_using_Stacks.py
+++ Implement_Queue_using_Stacks/Implement_Queue_using_Stacks.py
@@ -1,50 +1,85 @@
+"""
+Queue through stacks
+"""
 class Node:
+    """
+    Linked list node
+    """
     def __init__(self, value):
+        """initializes a node"""
         self.value = value
         self.next = None
 
 class Stack:
+    """
+    Linked stack (LIFO)
+    """
     def __init__(self):
         self.top_node = None
 
     def push(self, value):
+        """
+        Push a value on top of the stack
+        """
         new_node = Node(value)
         new_node.next = self.top_node
         self.top_node = new_node
 
     def pop(self):
+        """
+        Remove and return the top value
+        """
         value = self.top_node.value
         self.top_node = self.top_node.next
         return value
 
     def peek(self):
+        """
+        Return the top value
+        """
         return self.top_node.value
 
     def is_empty(self):
+        """
+        Return True if the stack is empty
+        """
         return self.top_node is None
 
 
 class MyQueue:
+    """FIFO queue using two stacks"""
     def __init__(self):
         self.in_stack = Stack()
         self.out_stack = Stack()
 
     def push(self, x: int) -> None:
+        """
+        Add an element to the back of the queue
+        """
         self.in_stack.push(x)
 
     def pop(self) -> int:
+        """
+        Remove and return the front element
+        """
         while self.out_stack.is_empty():
             while not self.in_stack.is_empty():
                 self.out_stack.push(self.in_stack.pop())
         return self.out_stack.pop()
 
     def peek(self) -> int:
+        """
+        Return the front element
+        """
         while self.out_stack.is_empty():
             while not self.in_stack.is_empty():
                 self.out_stack.push(self.in_stack.pop())
         return self.out_stack.peek()
 
     def empty(self) -> bool:
+        """
+        Return True if the queue is empty
+        """
         return self.in_stack.is_empty() and self.out_stack.is_empty()
 
 
--- Implement_Stack_using_Queue/Implement_Stack_using_Queues.py
+++ Implement_Stack_using_Queue/Implement_Stack_using_Queues.py
@@ -1,14 +1,26 @@
+"""
+LIFO stack implemented using two FIFO queues
+"""
 class Node:
+    """
+    Linked list node
+    """
     def __init__(self, value):
         self.value = value
         self.next = None
 
 class Queue:
+    """
+    Linked queue (FIFO)
+    """
     def __init__(self):
         self.front_node = None
         self.rear_node = None
 
     def enqueue(self, value):
+        """
+        Add an element to the back of the queue
+        """
         new_node = Node(value)
         if self.is_empty():
             self.front_node = new_node
@@ -18,6 +30,9 @@ class Queue:
             self.rear_node = new_node
 
     def dequeue(self):
+        """
+        Remove and return the front element
+        """
         value = self.front_node.value
         self.front_node = self.front_node.next
         if self.front_node is None:
@@ -25,30 +40,51 @@ class Queue:
         return value
 
     def peek(self):
+        """
+        Return the front element
+        """
         return self.front_node.value
 
     def is_empty(self):
+        """
+        Return True if the queue is empty
+        """
         return self.front_node is None
 
 
 class MyStack:
+    """
+    LIFO stack using two queues
+    """
     def __init__(self):
         self.q1 = Queue()
         self.q2 = Queue()
 
     def push(self, x: int) -> None:
+        """
+        Push an element on top of the stack
+        """
         self.q2.enqueue(x)
         while not self.q1.is_empty():
             self.q2.enqueue(self.q1.dequeue())
         self.q1, self.q2 = self.q2, self.q1
 
     def pop(self) -> int:
+        """
+        Remove and return the top element
+        """
         return self.q1.dequeue()
 
     def top(self) -> int:
+        """
+        Return the top element
+        """
         return self.q1.peek()
 
     def empty(self) -> bool:
+        """
+        Return True if the stack is empty
+        """
         return self.q1.is_empty()
 
 
