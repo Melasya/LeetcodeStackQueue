205f840dd40315894c603379140c4db6714846d2
Мелася <poliuha.pn@ucu.edu.ua>
2026-04-06 23:07:23 +0300

    Add new right solution and screenshot for Implement Queue using Stacks

--- Implement_Queue_using_Stacks/Implement_Queue_using_Stacks.py
+++ Implement_Queue_using_Stacks/Implement_Queue_using_Stacks.py
@@ -1,26 +1,51 @@
-class MyQueue:
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
+class Stack:
     def __init__(self):
-        self.in_stack = []
-        self.out_stack = []
+        self.top_node = None
 
-    def push(self, x):
-        self.in_stack.append(x)
+    def push(self, value):
+        new_node = Node(value)
+        new_node.next = self.top_node
+        self.top_node = new_node
 
     def pop(self):
-        while not self.out_stack:
-            while self.in_stack:
-                self.out_stack.append(self.in_stack.pop())
-        return self.out_stack.pop()
+        value = self.top_node.value
+        self.top_node = self.top_node.next
+        return value
 
     def peek(self):
-        while not self.out_stack:
-            while self.in_stack:
-                self.out_stack.append(self.in_stack.pop())
-        return self.out_stack[-1]
+        return self.top_node.value
 
-    def empty(self):
-        return not self.in_stack and not self.out_stack
+    def is_empty(self):
+        return self.top_node is None
+
+
+class MyQueue:
+    def __init__(self):
+        self.in_stack = Stack()
+        self.out_stack = Stack()
+
+    def push(self, x: int) -> None:
+        self.in_stack.push(x)
+
+    def pop(self) -> int:
+        while self.out_stack.is_empty():
+            while not self.in_stack.is_empty():
+                self.out_stack.push(self.in_stack.pop())
+        return self.out_stack.pop()
+
+    def peek(self) -> int:
+        while self.out_stack.is_empty():
+            while not self.in_stack.is_empty():
+                self.out_stack.push(self.in_stack.pop())
+        return self.out_stack.peek()
+
+    def empty(self) -> bool:
+        return self.in_stack.is_empty() and self.out_stack.is_empty()
 
 
 # Your MyQueue object will be instantiated and called as such:
@@ -28,4 +53,4 @@ class MyQueue:
 # obj.push(x)
 # param_2 = obj.pop()
 # param_3 = obj.peek()
-# param_4 = obj.empty()
+# param_4 = obj.empty
